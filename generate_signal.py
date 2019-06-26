91504498704b266df0f65c7f249fbf8ba8e9a96c
Liam Shepherd <liam@dark.ms>
2019-06-26 10:06:05 +0100

    Cleaned up basis transforms

--- generate_signal.py
+++ generate_signal.py
@@ -14,8 +14,9 @@ if not tf2.enabled():
 tfd = tfp.distributions
 
 # TODO: Check signal terms
-# TODO: Basis fixing
-# TODO: Consistent angle transforms
+# TODO: Switch to continuous PDF
+# TODO: Should J transforms be used? (Based on q2 >> ...)
+# TODO: Consistent angle transforms (trig vs pythag)
 # TODO: Convert to tf distribution?
 # TODO: Pip requirements.txt (& test)
 # TODO: Do fitting
@@ -24,19 +25,107 @@ tfd = tfp.distributions
 mass_mu = tf.constant(105.6583745e6)  # in 106 MeV/c^2
 q2_min = tf.constant(1.0e18)  # 1 (GeV/c^2)^2
 q2_max = tf.constant(6.0e18)  # 6 (GeV/c^2)^2
+one = tf.constant(1.0)
+two = tf.constant(2.0)
+four = tf.constant(4.0)
+
+# Outer arrays: [a_par_l, a_par_r, a_perp_l, a_perp_r, a_zero_l, a_zero_r]
+# Inner arrays: [Re(...), Im(...)
+# Inner array coeffs: [a, b, c] for anzatz a + (b * q2) + (c / q2)
+signal_coeffs = [
+    [[one, 0.0, 0.0], [two, 0.0, 0.0]],
+    [[four, 0.0, 0.0], [one, 0.0, 0.0]],
+    [[two, 0.0, 0.0], [four, 0.0, 0.0]],
+    [[one, 0.0, 0.0], [two, 0.0, 0.0]],
+    [[four, 0.0, 0.0], [one, 0.0, 0.0]],
+    [[two, 0.0, 0.0], [four, 0.0, 0.0]],
+]
 
 
-def decay_rate(independent_vars):
+def transform_amplitudes(original_amplitudes):
+    [a_par_l, a_par_r, a_perp_l, a_perp_r, a_zero_l, a_zero_r] = original_amplitudes
+
+    # Original basis
+    n_par = tf.Variable([a_par_l, tf.math.conj(a_par_r)])
+    n_perp = tf.Variable([a_perp_l, - tf.math.conj(a_perp_r)])
+    n_zero = tf.Variable([a_zero_l, tf.math.conj(a_zero_r)])
+
+    # Transformation angle omega
+    tan_2omega = two * (
+            (tf.math.imag(a_zero_r) * tf.math.real(a_zero_l)) + (tf.math.imag(a_zero_l) * tf.math.real(a_zero_r))
+    ) / (tf.math.abs(a_zero_r) ** 2 - tf.math.abs(a_zero_l) ** 2)
+    omega = tf.math.atan(tan_2omega) / two
+    tan_omega = tf.math.tan(omega)
+    sinh_iomega = tf.cast(tf.math.sinh(tf.complex(0.0, omega)), tf.complex64)
+    cosh_iomega = tf.cast(tf.math.cosh(tf.complex(0.0, omega)), tf.complex64)
+
+    # Transformation angle theta
+    tan_theta = (tf.math.real(a_zero_r) + (tf.math.imag(a_zero_l) * tan_omega)) / \
+                (- tf.math.real(a_zero_l) + (tf.math.imag(a_zero_r) * tan_omega))
+    theta = tf.math.atan(tan_theta)
+    sin_theta = tf.cast(tf.math.sin(theta), tf.complex64)
+    cos_theta = tf.cast(tf.math.cos(theta), tf.complex64)
+
+    # Transformation angle phi_l
+    tan_phi_l = (
+            tf.math.imag(a_zero_l) +
+            (tf.math.imag(a_zero_r) * tan_theta) -
+            ((tf.math.real(a_zero_r) - (tf.math.real(a_zero_l) * tan_theta)) * tan_omega)
+    ) / (
+            - tf.math.real(a_zero_l) +
+            (tf.math.real(a_zero_r) * tan_theta) +
+            ((tf.math.imag(a_zero_r) + (tf.math.imag(a_zero_l) * tan_theta)) * tan_omega)
+    )
+    phi_l = tf.math.atan(tan_phi_l)
+    exp_iphi_l = tf.cast(tf.math.exp(tf.complex(0.0, phi_l)), tf.complex64)
+
+    # Transformation angle phi_r
+    tan_phi_r = (
+            tf.math.imag(a_perp_r) +
+            (tf.math.imag(a_perp_l) * tan_theta) -
+            ((tf.math.real(a_perp_l) - (tf.math.real(a_perp_r) * tan_theta)) * tan_omega)
+    ) / (
+            - tf.math.real(a_perp_r) +
+            (tf.math.real(a_perp_l) * tan_theta) +
+            ((tf.math.imag(a_perp_l) + (tf.math.imag(a_perp_r) * tan_theta)) * tan_omega)
+    )
+    phi_r = tf.math.atan(tan_phi_r)
+    exp_miphi_r = tf.cast(tf.math.exp(tf.complex(0.0, - phi_r)), tf.complex64)
+
+    # Transform basis
+    def _transform_basis(n):
+        # FIXME: Change this to operation on matrices directly
+        return tf.Variable([
+            exp_iphi_l * (
+                    (cos_theta * ((n[0] * cosh_iomega) - n[1] * sinh_iomega)) -
+                    (- sin_theta * ((- n[0] * sinh_iomega) + n[1] * cosh_iomega))
+            ),
+            exp_miphi_r * (
+                    (sin_theta * ((n[0] * cosh_iomega) - n[1] * sinh_iomega)) -
+                    (cos_theta * ((- n[0] * sinh_iomega) + n[1] * cosh_iomega))
+            )
+        ])
+    nt_par = _transform_basis(n_par)
+    nt_perp = _transform_basis(n_perp)
+    nt_zero = _transform_basis(n_zero)
+
+    # Transformed amplitudes (w/ basis fixing)
+    a_par_l = nt_par[0]
+    a_par_r = tf.math.conj(nt_par[1])
+    a_perp_l = nt_perp[0]
+    a_perp_r = tf.complex(- tf.math.real(nt_perp[1]), 0.0)
+    a_zero_l = tf.complex(tf.math.real(nt_zero[0]), 0.0)
+    a_zero_r = tf.constant(tf.complex(0.0, 0.0), shape=[a_zero_l.get_shape()[0]])
+
+    return [a_par_l, a_par_r, a_perp_l, a_perp_r, a_zero_l, a_zero_r]
+
+
+def decay_rate(independent_vars, amplitudes):
     q2 = independent_vars[:, 0]
     cos_theta_k = independent_vars[:, 1]
     cos_theta_l = independent_vars[:, 2]
     phi = independent_vars[:, 3]
-    num_rows = independent_vars.get_shape()[0]
-
-    # Constants
-    one = tf.constant(1.0)
-    two = tf.constant(2.0)
-    four = tf.constant(4.0)
+    [a_par_l, a_par_r, a_perp_l, a_perp_r, a_zero_l, a_zero_r] = amplitudes
 
     # Angles
     cos2_theta_k = cos_theta_k**2
@@ -59,97 +148,6 @@ def decay_rate(independent_vars):
     four_mass_mu_over_q2 = (four * (mass_mu**2)) / q2
     beta2_mu = one - four_mass_mu_over_q2
     beta_mu = tf.sqrt(beta2_mu)
-
-    # Amplitudes
-    a_par_l = tf.complex(one + (0 * q2) + (0 / q2), two + (0 * q2) + (0 / q2))
-    a_par_r = tf.complex(four + (0 * q2) + (0 / q2), one + (0 * q2) + (0 / q2))
-    a_perp_l = tf.complex(two + (0 * q2) + (0 / q2), four + (0 * q2) + (0 / q2))
-    a_perp_r = tf.complex(one + (0 * q2) + (0 / q2), two + (0 * q2) + (0 / q2))
-    a_zero_l = tf.complex(four + (0 * q2) + (0 / q2), one + (0 * q2) + (0 / q2))
-    a_zero_r = tf.complex(two + (0 * q2) + (0 / q2), four + (0 * q2) + (0 / q2))
-
-    # Transformation angles
-    tan_2omega = two * (
-            (tf.math.imag(a_zero_r) * tf.math.real(a_zero_l)) + (tf.math.imag(a_zero_l) * tf.math.real(a_zero_r))
-    ) / (tf.math.abs(a_zero_r)**2 - tf.math.abs(a_zero_l)**2)
-    omega = tf.math.atan(tan_2omega) / two
-    tan_omega = tf.math.tan(omega)
-    sinh_iomega = tf.cast(tf.math.sinh(tf.complex(0.0, omega)), tf.complex64)
-    cosh_iomega = tf.cast(tf.math.cosh(tf.complex(0.0, omega)), tf.complex64)
-
-    tan_theta = (tf.math.real(a_zero_r) + (tf.math.imag(a_zero_l) * tan_omega)) / \
-        (- tf.math.real(a_zero_l) + (tf.math.imag(a_zero_r) * tan_omega))
-    theta = tf.math.atan(tan_theta)
-    sin_theta = tf.cast(tf.math.sin(theta), tf.complex64)
-    cos_theta = tf.cast(tf.math.cos(theta), tf.complex64)
-
-    tan_phi_l = (
-            tf.math.imag(a_zero_l) +
-            (tf.math.imag(a_zero_r) * tan_theta) -
-            ((tf.math.real(a_zero_r) - (tf.math.real(a_zero_l) * tan_theta)) * tan_omega)
-    ) / (
-        - tf.math.real(a_zero_l) +
-        (tf.math.real(a_zero_r) * tan_theta) +
-        ((tf.math.imag(a_zero_r) + (tf.math.imag(a_zero_l) * tan_theta)) * tan_omega)
-    )
-    phi_l = tf.math.atan(tan_phi_l)
-    exp_iphi_l = tf.cast(tf.math.exp(tf.complex(0.0, phi_l)), tf.complex64)
-
-    tan_phi_r = (
-            tf.math.imag(a_perp_r) +
-            (tf.math.imag(a_perp_l) * tan_theta) -
-            ((tf.math.real(a_perp_l) - (tf.math.real(a_perp_r) * tan_theta)) * tan_omega)
-    ) / (
-            - tf.math.real(a_perp_r) +
-            (tf.math.real(a_perp_l) * tan_theta) +
-            ((tf.math.imag(a_perp_l) + (tf.math.imag(a_perp_r) * tan_theta)) * tan_omega)
-    )
-    phi_r = tf.math.atan(tan_phi_r)
-    exp_miphi_r = tf.cast(tf.math.exp(tf.complex(0.0, - phi_r)), tf.complex64)
-
-    # Basis vectors
-    n_par = tf.Variable([a_par_l, tf.math.conj(a_par_r)])
-    n_perp = tf.Variable([a_perp_l, - tf.math.conj(a_perp_r)])
-    n_zero = tf.Variable([a_zero_l, tf.math.conj(a_zero_r)])
-
-    # Transform matrices
-    # t1 = tf.Variable([
-    #     [exp_iphi_l, tf.constant(tf.complex(0.0, 0.0), shape=[num_rows])],
-    #     [tf.constant(tf.complex(0.0, 0.0), shape=[num_rows]), exp_miphi_r]
-    # ])
-    # t2 = tf.Variable([[cos_theta, - sin_theta], [sin_theta, cos_theta]], dtype=tf.complex64)
-    # t3 = tf.Variable([[cosh_iomega, - sinh_iomega], [-sinh_iomega, cosh_iomega]])
-
-    # Transforms
-    def _transform(n):
-        # x = tf.tensordot(t3, n, axes=[[1, 2], [0, 1]])
-
-        # x = tf.tensordot(t2, x, axes=[[2], [0]])
-        # x = tf.tensordot(t1, x, axes=[[2], [0]])
-        # return tf.tensordot(t1, tf.tensordot(t2, tf.tensordot(t3, n, axes=[[2], [0]]), axes=[[2], [0]]), axes=[[2], [0]])
-        return tf.Variable([
-            exp_iphi_l * (
-                (cos_theta * ((n[0] * cosh_iomega) - n[1] * sinh_iomega)) -
-                (- sin_theta * ((- n[0] * sinh_iomega) + n[1] * cosh_iomega))
-            ),
-            exp_miphi_r * (
-                    (sin_theta * ((n[0] * cosh_iomega) - n[1] * sinh_iomega)) -
-                    (cos_theta * ((- n[0] * sinh_iomega) + n[1] * cosh_iomega))
-            )
-        ])
-
-    # Transformed basis
-    nt_par = _transform(n_par)
-    nt_perp = _transform(n_perp)
-    nt_zero = _transform(n_zero)
-
-    # Transformed amplitudes (w/ basis fixing)
-    a_par_l = nt_par[0]
-    a_par_r = tf.math.conj(nt_par[1])
-    a_perp_l = nt_perp[0]
-    a_perp_r = tf.complex(- tf.math.real(nt_perp[1]), 0.0)
-    a_zero_l = tf.complex(tf.math.real(nt_zero[0]), 0.0)
-    a_zero_r = tf.constant(tf.complex(0.0, 0.0), shape=[num_rows])
 
     # Angular observables
     j_1s = ((two + beta2_mu) / four)*(
@@ -229,9 +227,10 @@ def generate_signal(signal_samples, options_num):
     def _print(name, t):
         tf.print(name, "(shape", tf.shape(t), "):\n", t, output_stream=sys.stdout, end="\n\n")
 
+    q2 = q2_distribution.sample(options_num)
     options = tf.stack(
         [
-            q2_distribution.sample(options_num),
+            q2,
             cos_theta_k_distribution.sample(options_num),
             cos_theta_l_distribution.sample(options_num),
             phi_distribution.sample(options_num)
@@ -241,7 +240,19 @@ def generate_signal(signal_samples, options_num):
     )
     _print("options", options)
 
-    decay_rates = decay_rate(options)
+    # Signal values for original amplitudes
+    signal_amplitudes = []
+    for amplitude_coeffs in signal_coeffs:
+        components = []
+        for component_coeffs in amplitude_coeffs:
+            components.append(component_coeffs[0] + (component_coeffs[1] * q2) + (component_coeffs[2] / q2))
+        signal_amplitudes.append(tf.complex(components[0], components[1]))
+    _print("signal_amplitudes", signal_amplitudes)
+
+    transformed_amplitudes = transform_amplitudes(signal_amplitudes)
+    _print("transformed_amplitudes", transformed_amplitudes)
+
+    decay_rates = decay_rate(options, transformed_amplitudes)
     _print("decay_rates", decay_rates)
 
     total_decay_rate = tf.reduce_sum(decay_rates)
@@ -259,7 +270,7 @@ def generate_signal(signal_samples, options_num):
     return signal
 
 
-s = generate_signal(10_000, 10_000_000)
+s = generate_signal(100_000, 10_000_000)
 
 fig, axes = plt.subplots(nrows=2, ncols=2)
 fig.suptitle('Signal distributions')

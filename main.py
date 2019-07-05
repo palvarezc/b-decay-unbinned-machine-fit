Split coefficients into new file Moved more operations to GPU Fixed nll sign Added @tf.function to PDF Added profiling script

main.py
@@ -1,127 +1,65 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
+import sys
 
-from signal_distribution import generate_events, pdf
+from coefficients import *
+from signal_distribution import generate_events, negative_log_likelihood
 
 import tensorflow.compat.v2 as tf
 tf.enable_v2_behavior()
 
 # tf.debugging.set_log_device_placement(True)
 
-# TODO: Fix basis fitting
-# TODO: Fix fitting/Check one or two parameter fits
+# TODO: Optimise integral
 # TODO: Check maths terms
+
 # TODO: Switch to accept-reject/monte-carlo. Increase sample size
-# TODO: Add fitting graphs
-# TODO: Do ensembles & plot distributions
-# TODO: Optimise hyperparameters, optimiser, model
-# TODO: Split files/Keras?/Convert to tf distribution?/Unit tests/method params/comments/doc comments
+# TODO: Fix fitting/Check one or two parameter fits+surfaces
 
-zero = tf.constant(0.0)
+# TODO: Fix basis fitting/Check
 
-# Outer arrays: [a_par_l, a_par_r, a_perp_l, a_perp_r, a_zero_l, a_zero_r]
-# Inner arrays: [Re(...), Im(...)
-# Inner array coeffs: [a, b, c] for anzatz a + (b * q2) + (c / q2)
-signal_coeffs = [
-    [
-        [-3.4277495848061257, -0.12410026985551571, 6.045281152442963],
-        [0.00934061365013997, -0.001989193837745718, 0.5034113300277555]
-    ],
-    [
-        [-0.25086978961912654, -0.005180213333933305, 8.636744983192575],
-        [0.2220926359265556, -0.017419352926410284, -0.528067287659531]
-    ],
-    [
-        [3.0646407176207813, 0.07851536717584778, -8.841144517240298],
-        [-0.11366033229864046, 0.009293559978293, 0.04761546602270795]
-    ],
-    [
-        [-0.9332669880450042, 0.01686711151445955, -6.318555350023665],
-        [zero, zero, zero]
-    ],
-    [
-        [5.882883042792871, -0.18442496620391777, 8.10139804649606],
-        [zero, zero, zero]
-    ],
-    [
-        [zero, zero, zero],
-        [zero, zero, zero]
-    ],
-]
-
-
-def build_coeff_structure(flat_coeffs):
-    return [
-        [flat_coeffs[0:3],   flat_coeffs[3:6]],
-        [flat_coeffs[6:9],   flat_coeffs[9:12]],
-        [flat_coeffs[12:15], flat_coeffs[15:18]],
-        [flat_coeffs[18:21], [zero, zero, zero]],
-        [flat_coeffs[21:24], [zero, zero, zero]],
-        [[zero, zero, zero], [zero, zero, zero]],
-    ]
-
-
-def coeffs_to_string(coeffs):
-    coeffs_strs = []
-    for amp_coeffs in coeffs:
-        for comp_coeffs in amp_coeffs:
-            for coeff in comp_coeffs:
-                if coeff is zero:
-                    coeffs_strs.append(' *0.0*')
-                else:
-                    coeffs_strs.append('{:6.2f}'.format(coeff.numpy() if hasattr(coeff, 'numpy') else coeff))
-    return ' '.join(coeffs_strs)
-
-
-def nll(signal_events_, fit_coeffs_):
-    coeff_struct_ = build_coeff_structure(fit_coeffs_)
-    probs_ = pdf(signal_events_, coeff_struct_)
-    nlog_probs = -tf.math.log(probs_)
-    nll_ = -tf.reduce_sum(nlog_probs) / signal_events_.get_shape()[0]
-    return nll_
-
+# TODO: Do ensembles & plot distributions. Change to proper sample size
+# TODO: Optimise hyperparameters, choose optimiser, more layers? Keras?
+# TODO: Convert to tf distribution?/Unit tests/comments/doc comments
 
 #######################
 
+
+def plot_signal(events):
+    fig, axes = plt.subplots(nrows=2, ncols=2)
+    fig.suptitle('Signal distributions')
+    titles = [
+        r'$q^2$',
+        r'$\cos{\theta_k}$',
+        r'$\cos{\theta_l}$',
+        r'$\phi$'
+    ]
+
+    for ax, feature, title in zip(axes.flatten(), events.numpy().transpose(), titles):
+        sns.distplot(feature, ax=ax, bins=20)
+        ax.set(title=title)
+
+    plt.show()
+
+
 signal_events = generate_events(100_000, signal_coeffs)
-
-fig, axes = plt.subplots(nrows=2, ncols=2)
-fig.suptitle('Signal distributions')
-titles = [
-    r'$q^2$',
-    r'$\cos{\theta_k}$',
-    r'$\cos{\theta_l}$',
-    r'$\phi$'
-]
-
-for ax, feature, title in zip(axes.flatten(), signal_events.numpy().transpose(), titles):
-    sns.distplot(feature, ax=ax, bins=20)
-    ax.set(title=title)
-
-plt.show()
+plot_signal(signal_events)
 
 ######################
 
-fit_coeffs = [tf.Variable(1.0, name='c{0}'.format(i)) for i in range(24)]
 
-# optimizer = tf.optimizers.Nadam(learning_rate=0.5)
-optimizer = tf.optimizers.Adam(learning_rate=0.01)
-# optimizer = tf.optimizers.SGD(learning_rate=0.01, momentum=0.01)
+def print_step(step):
+    tf.print("Step:", step, "nll:", negative_log_likelihood(signal_events, fit_coeffs), output_stream=sys.stdout)
+    tf.print("fit:   ", coeffs_to_string(fit_coeffs), output_stream=sys.stdout)
+    tf.print("signal:", coeffs_to_string(signal_coeffs), output_stream=sys.stdout)
 
-# import timeit
-# print("nll x 1000: ", timeit.timeit(lambda: nll(signal_events, fit_coeffs), number=1000))
-# exit(0)
 
-print("Initial nll: {:.3f}".format(nll(signal_events, fit_coeffs)))
+print_step("initial")
+optimizer = tf.optimizers.Nadam(learning_rate=0.01)
 
-# Training loop
 for i in range(10000):
-    optimizer.minimize(
-        lambda: nll(signal_events, fit_coeffs),
-        var_list=fit_coeffs
-    )
+    optimizer.minimize(lambda: negative_log_likelihood(signal_events, fit_coeffs), var_list=trainable_coeffs)
     if i % 20 == 0:
-        print("Step {:03d}. nll: {:.3f}".format(i, nll(signal_events, fit_coeffs)))
-        print("fit:    {}".format(coeffs_to_string(build_coeff_structure(fit_coeffs))))
-        print("signal: {}".format(coeffs_to_string(signal_coeffs)))
+        print_step(i)
 
+print_step("final")

signal_distribution.py
@@ -17,36 +17,42 @@ four = tf.constant(4.0)
 options_num = 10_000_000
 
 
-# A @tf.function decoration makes this 3-3.5x faster, however as odeint() is not Tensorflow v2
-#  compatible, after 1000s of iterations the gradients can all come randomly back as NaN. Very sad.
-#  This error manifests itself as odeint complaining of a float (dt) underflow.
-#  If odeint() is ported to tf_scientific then remove the decoration from child functions and add it here.
-def pdf(signal_events_, coeffs):
-    # Forcing the GPU here is about 25% faster. Might no longer be needed with @tf.function decoration
+@tf.function
+def pdf(events, coeffs):
     with tf.device('/device:GPU:0'):
-        decay_rates = _decay_rate(signal_events_, coeffs)
+        decay_rates = _decay_rate(events, coeffs)
         norm = _integrate_decay_rate(coeffs)
-    return tf.math.maximum(decay_rates / norm, 1e-30)
-
-
-def generate_events(events_num, coeffs):
-    q2 = tfp.distributions.Uniform(low=q2_min, high=q2_max).sample(options_num)
-    cos_theta_k = tfp.distributions.Uniform(low=-1.0, high=1.0).sample(options_num)
-    cos_theta_l = tfp.distributions.Uniform(low=-1.0, high=1.0).sample(options_num)
-    phi = tfp.distributions.Uniform(low=-math.pi, high=math.pi).sample(options_num)
-
-    options = tf.stack([q2, cos_theta_k, cos_theta_l, phi], axis=1, name='signal_options')
-
-    probs = pdf(options, coeffs)
-    normalised_probs = pdf(options, coeffs) / tf.reduce_sum(probs)
-    choices = np.random.choice(options.get_shape()[0], events_num, p=normalised_probs.numpy())
-
-    return tf.gather(options, choices)
+        return tf.math.maximum(decay_rates / norm, 1e-30)
 
 
 @tf.function
-def _decay_rate(signal_events_, coeffs):
-    [q2, cos_theta_k, cos_theta_l, phi] = tf.unstack(signal_events_, axis=1)
+def negative_log_likelihood(events, coeffs):
+    with tf.device('/device:GPU:0'):
+        return -tf.reduce_sum(
+            tf.math.log(
+                pdf(events, coeffs)
+            )
+        )
+
+
+def generate_events(events_num, coeffs):
+    with tf.device('/device:GPU:0'):
+        q2 = tfp.distributions.Uniform(low=q2_min, high=q2_max).sample(options_num)
+        cos_theta_k = tfp.distributions.Uniform(low=-1.0, high=1.0).sample(options_num)
+        cos_theta_l = tfp.distributions.Uniform(low=-1.0, high=1.0).sample(options_num)
+        phi = tfp.distributions.Uniform(low=-math.pi, high=math.pi).sample(options_num)
+
+        options = tf.stack([q2, cos_theta_k, cos_theta_l, phi], axis=1)
+
+        probs = pdf(options, coeffs)
+        normalised_probs = pdf(options, coeffs) / tf.reduce_sum(probs)
+        choices = np.random.choice(options.get_shape()[0], events_num, p=normalised_probs.numpy())
+
+        return tf.gather(options, choices, name='events')
+
+
+def _decay_rate(events, coeffs):
+    [q2, cos_theta_k, cos_theta_l, phi] = tf.unstack(events, axis=1)
     amplitudes = _coeffs_to_amplitudes(q2, coeffs)
 
     # Angles
@@ -101,7 +107,6 @@ def _decay_rate(signal_events_, coeffs):
 
 # https://arxiv.org/abs/1202.4266
 # @see notebook
-@tf.function
 def _decay_rate_angle_integrated(q2, coeffs):
     amplitudes = _coeffs_to_amplitudes(q2, coeffs)
 
@@ -132,13 +137,13 @@ def _beta2(four_mass2_over_q2):
 
 
 def _j1s(amplitudes, beta2_mu, four_mass2_over_q2):
-    [a_par_l, a_par_r, a_perp_l, a_perp_r, _, _] = amplitudes
+    [a_para_l, a_para_r, a_perp_l, a_perp_r, _, _] = amplitudes
     return ((two + beta2_mu) / four) * (
-        tf.math.abs(a_perp_l) ** 2 + tf.math.abs(a_par_l) ** 2 +
-        tf.math.abs(a_perp_r) ** 2 + tf.math.abs(a_par_r) ** 2
+        tf.math.abs(a_perp_l) ** 2 + tf.math.abs(a_para_l) ** 2 +
+        tf.math.abs(a_perp_r) ** 2 + tf.math.abs(a_para_r) ** 2
     ) + four_mass2_over_q2 * tf.math.real(
         a_perp_l * tf.math.conj(a_perp_r) +
-        a_par_l * tf.math.conj(a_par_r)
+        a_para_l * tf.math.conj(a_para_r)
     )
 
 
@@ -149,10 +154,10 @@ def _j1c(amplitudes, four_mass2_over_q2):
 
 
 def _j2s(amplitudes, beta2_mu):
-    [a_par_l, a_par_r, a_perp_l, a_perp_r, _, _] = amplitudes
+    [a_para_l, a_para_r, a_perp_l, a_perp_r, _, _] = amplitudes
     return (beta2_mu / four) * (
-        tf.math.abs(a_perp_l) ** 2 + tf.math.abs(a_par_l) ** 2 +
-        tf.math.abs(a_perp_r) ** 2 + tf.math.abs(a_par_r) ** 2
+        tf.math.abs(a_perp_l) ** 2 + tf.math.abs(a_para_l) ** 2 +
+        tf.math.abs(a_perp_r) ** 2 + tf.math.abs(a_para_r) ** 2
     )
 
 
@@ -162,18 +167,18 @@ def _j2c(amplitudes, beta2_mu):
 
 
 def _j3(amplitudes, beta2_mu):
-    [a_par_l, a_par_r, a_perp_l, a_perp_r, _, _] = amplitudes
+    [a_para_l, a_para_r, a_perp_l, a_perp_r, _, _] = amplitudes
     return (beta2_mu / two) * (
-        tf.math.abs(a_perp_l) ** 2 - tf.math.abs(a_par_l) ** 2 +
-        tf.math.abs(a_perp_r) ** 2 - tf.math.abs(a_par_r) ** 2
+        tf.math.abs(a_perp_l) ** 2 - tf.math.abs(a_para_l) ** 2 +
+        tf.math.abs(a_perp_r) ** 2 - tf.math.abs(a_para_r) ** 2
     )
 
 
 def _j4(amplitudes, beta2_mu):
-    [a_par_l, a_par_r, _, _, a_zero_l, a_zero_r] = amplitudes
+    [a_para_l, a_para_r, _, _, a_zero_l, a_zero_r] = amplitudes
     return (beta2_mu / tf.sqrt(two)) * (
-        tf.math.real(a_zero_l * tf.math.conj(a_par_l)) +
-        tf.math.real(a_zero_r * tf.math.conj(a_par_r))
+        tf.math.real(a_zero_l * tf.math.conj(a_para_l)) +
+        tf.math.real(a_zero_r * tf.math.conj(a_para_r))
     )
 
 
@@ -186,18 +191,18 @@ def _j5(amplitudes, beta_mu):
 
 
 def _j6s(amplitudes, beta_mu):
-    [a_par_l, a_par_r, a_perp_l, a_perp_r, _, _] = amplitudes
+    [a_para_l, a_para_r, a_perp_l, a_perp_r, _, _] = amplitudes
     return two * beta_mu * (
-        tf.math.real(a_par_l * tf.math.conj(a_perp_l)) -
-        tf.math.real(a_par_r * tf.math.conj(a_perp_r))
+        tf.math.real(a_para_l * tf.math.conj(a_perp_l)) -
+        tf.math.real(a_para_r * tf.math.conj(a_perp_r))
     )
 
 
 def _j7(amplitudes, beta_mu):
-    [a_par_l, a_par_r, _, _, a_zero_l, a_zero_r] = amplitudes
+    [a_para_l, a_para_r, _, _, a_zero_l, a_zero_r] = amplitudes
     return tf.sqrt(two) * beta_mu * (
-        tf.math.imag(a_zero_l * tf.math.conj(a_par_l)) -
-        tf.math.imag(a_zero_r * tf.math.conj(a_par_r))
+        tf.math.imag(a_zero_l * tf.math.conj(a_para_l)) -
+        tf.math.imag(a_zero_r * tf.math.conj(a_para_r))
     )
 
 
@@ -210,10 +215,10 @@ def _j8(amplitudes, beta2_mu):
 
 
 def _j9(amplitudes, beta2_mu):
-    [a_par_l, a_par_r, a_perp_l, a_perp_r, _, _] = amplitudes
+    [a_para_l, a_para_r, a_perp_l, a_perp_r, _, _] = amplitudes
     return beta2_mu * (
-        tf.math.imag(tf.math.conj(a_par_l) * a_perp_l) +
-        tf.math.imag(tf.math.conj(a_par_r) * a_perp_r)
+        tf.math.imag(tf.math.conj(a_para_l) * a_perp_l) +
+        tf.math.imag(tf.math.conj(a_para_r) * a_perp_r)
     )
 
 
@@ -228,11 +233,10 @@ def _integrate_decay_rate(coeffs):
 
 
 def _coeffs_to_amplitudes(q2, coeffs):
-    amplitudes = []
-    for amplitude_coeffs in coeffs:
-        components = []
-        for component_coeffs in amplitude_coeffs:
-            components.append(component_coeffs[0] + (component_coeffs[1] * q2) + (component_coeffs[2] / q2))
-        amplitudes.append(tf.complex(components[0], components[1]))
+    def _anzatz(c):
+        return c[0] + (c[1] * q2) + (c[2] / q2)
 
-    return amplitudes
+    def _amplitude(c):
+        return tf.complex(_anzatz(c[0:3]), _anzatz(c[3:6]))
+
+    return [_amplitude(coeffs[i:i+6]) for i in range(0, len(coeffs), 6)]
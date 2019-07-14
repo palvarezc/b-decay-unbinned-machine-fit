02872bed683d4f9a5380871b2e139495d5ef8b24
Liam Shepherd <liam@dark.ms>
2019-07-14 12:20:03 +0100

    Added comments to likelihood scripts

--- bin/likelihood_contours.py
+++ bin/likelihood_contours.py
@@ -16,14 +16,18 @@ tf.enable_v2_behavior()
 cx_idx = 2
 cy_idx = 5
 grid_points = 50
+grid_min = -12.0
+grid_max = 12.0
 
 
+# Use autograph for performance
 @tf.function
 def nll(coeffs_, signal_events_):
     return bmf.signal.normalized_nll(coeffs_, signal_events_)
 
 
 def try_nll(c):
+    # Override the two coeffs we want to vary
     fit_coeffs[cx_idx] = tf.constant(c[0])
     fit_coeffs[cy_idx] = tf.constant(c[1])
     return nll(fit_coeffs, signal_events)
@@ -32,26 +36,33 @@ def try_nll(c):
 with bmf.Script():
     signal_coeffs = bmf.coeffs.signal()
     signal_events = bmf.signal.generate(signal_coeffs)
+    # Set fit coeffs to our constant signal ones
     fit_coeffs = bmf.coeffs.signal()
 
-    cx = np.linspace(-12.0, 12.0, grid_points, dtype=np.float32)
-    cy = np.linspace(-12.0, 12.0, grid_points, dtype=np.float32)
+    cx = np.linspace(grid_min, grid_max, grid_points, dtype=np.float32)
+    cy = np.linspace(grid_min, grid_max, grid_points, dtype=np.float32)
     X, Y = tf.meshgrid(cx, cy)
 
     with tf.device('/device:GPU:0'):
         points_grid = tf.stack([X, Y], axis=2)
+        # Turn our grid into (x, y) pairs
         points = tf.reshape(points_grid, [grid_points ** 2, 2])
+        # Calculate likelihoods
         likelihoods = tf.map_fn(try_nll, points)
+        # Convert likelihoods back into meshgrid shape
         likelihoods_grid = tf.reshape(likelihoods, [grid_points, grid_points])
 
     fig, ax = plt.subplots()
     CS = ax.contour(X, Y, likelihoods_grid)
-    ax.set_title('Likelihood')
+
+    # Label contours
     ax.clabel(CS, inline=1, fontsize=10)
 
+    # Label axes
     ax.set_xlabel(bmf.coeffs.latex_names[cx_idx])
     ax.set_ylabel(bmf.coeffs.latex_names[cy_idx])
 
+    # Draw X/Y signal lines
     ax.axvline(signal_coeffs[cx_idx].numpy(), color='r')
     ax.axhline(signal_coeffs[cy_idx].numpy(), color='r')
 
--- bin/likelihood_curves.py
+++ bin/likelihood_curves.py
@@ -13,13 +13,19 @@ import b_meson_fit as bmf
 
 tf.enable_v2_behavior()
 
+plot_points = 50
+plot_min = -12.0
+plot_max = 12.0
 
+
+# Use autograph for performance
 @tf.function
 def nll(coeffs_, signal_events_):
     return bmf.signal.normalized_nll(coeffs_, signal_events_)
 
 
 def try_nll(pos_, val):
+    # Override the coeff we want to vary
     try_coeffs[pos_] = tf.constant(val)
     return nll(try_coeffs, signal_events)
 
@@ -29,24 +35,32 @@ with bmf.Script() as script:
     signal_events = bmf.signal.generate(signal_coeffs)
     fit_coeffs = bmf.coeffs.fit()
 
+    # For each amplitude
     for a_idx in range(0, bmf.coeffs.amplitude_count):
+        # If this amplitude is fixed in this basis then ignore it
         if not bmf.coeffs.is_trainable(fit_coeffs[a_idx*bmf.coeffs.param_count]):
             continue
 
         fig, axes = plt.subplots(bmf.coeffs.param_count)
         fig.suptitle(bmf.coeffs.amplitude_latex_names[a_idx])
 
+        # For each param in this amplitude
         for p_idx in range(0, bmf.coeffs.param_count):
             c_idx = a_idx * bmf.coeffs.param_count + p_idx
-            script.stdout('Processing {} ({})'.format(bmf.coeffs.names[c_idx], c_idx))
+            bmf.stdout('Processing {} ({})'.format(bmf.coeffs.names[c_idx], c_idx))
+
+            # Set all coeffs to the constant signal ones
             try_coeffs = bmf.coeffs.signal()
 
-            c_range = np.linspace(-12.0, 12.0, 100, dtype=np.float32)
+            c_range = np.linspace(plot_min, plot_max, plot_points, dtype=np.float32)
 
             with tf.device('/device:GPU:0'):
                 axes[p_idx].plot(c_range, list(map(lambda c_val: try_nll(c_idx, c_val).numpy(), c_range)))
 
+            # Add the param's greek letter on the Y-axis
             axes[p_idx].set_ylabel(bmf.coeffs.param_latex_names[p_idx])
+
+            # Draw a red line to represent the signal
             axes[p_idx].axvline(signal_coeffs[c_idx].numpy(), ymax=0.5, color='r')
 
         plt.show()

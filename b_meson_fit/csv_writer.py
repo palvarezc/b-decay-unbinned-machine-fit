518b67d38fa9bebcf7fad0a633af555476d19db1
Liam Shepherd <liam@dark.ms>
2019-07-16 12:40:42 +0100

    Added comments to CSV writer

--- b_meson_fit/csv_writer.py
+++ b_meson_fit/csv_writer.py
@@ -1,3 +1,11 @@
+"""Write coefficients to a CSV file for ensamble runs.
+
+File will contain columns id,a_para_l_re_alpha,...,a_zero_r_im_gamma
+
+Writer will append to a current file if it exists.
+
+ID is automatic and will be incremented for each row.
+"""
 import csv
 import os
 
@@ -8,16 +16,22 @@ class CsvWriter:
     current_id = 0
     handle = None
 
-    def __init__(self, filename):
+    def __init__(self, file_path):
+        """Args:
+            file_path (str): Full path of CSV to write
+        """
         written_headers = False
-        if os.path.isfile(filename):
-            with open(filename, 'r', newline='') as previous_csv:
+
+        # If we already have a file then assume headers are written and get latest ID
+        if os.path.isfile(file_path):
+            with open(file_path, 'r', newline='') as previous_csv:
                 rows = csv.DictReader(previous_csv)
                 written_headers = True
                 for row in rows:
                     self.current_id = int(row['id'])
 
-        self.handle = open(filename, "a", newline='')
+        # Open the file for writing and write headers if we haven't written before
+        self.handle = open(file_path, "a", newline='')
         self.writer = csv.DictWriter(self.handle, fieldnames=(['id'] + bmfc.names))
         if not written_headers:
             self.writer.writeheader()
@@ -27,6 +41,11 @@ class CsvWriter:
             self.handle.close()
 
     def write_coeffs(self, coeffs):
+        """Write row of coefficients
+
+        Args:
+            coeffs (list of tensors): Coefficient list to write
+        """
         self.current_id = self.current_id + 1
         coeff_floats = [c.numpy() for c in coeffs]
         row = {'id': self.current_id, **dict(zip(bmfc.names, coeff_floats))}

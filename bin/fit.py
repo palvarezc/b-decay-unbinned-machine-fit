e1584c3a82f6f8dec222d00e678f5beef457b608
Liam Shepherd <liam@dark.ms>
2019-07-16 18:23:23 +0100

    Fixed bug where signal coeffs would get regenerated for a fit retry

--- bin/fit.py
+++ bin/fit.py
@@ -30,16 +30,15 @@ with bmf.Script() as script:
         exit(0)
 
     while writer.current_id < iterations:
-        bmf.stdout('Starting iteration {}/{}'.format(writer.current_id + 1, iterations))
+        iteration = writer.current_id + 1
+        bmf.stdout('Starting iteration {}/{}'.format(iteration, iterations))
 
         signal_coeffs = bmf.coeffs.signal()
-        fit_coeffs = bmf.coeffs.fit()
-
         signal_events = bmf.signal.generate(signal_coeffs, events_total=2500)
 
         # Plot our signal distributions for each independent variable
         fig, axes = plt.subplots(nrows=2, ncols=2)
-        fig.suptitle('Signal distributions')
+        fig.suptitle('Signal (Iteration {}/{})'.format(iteration, iterations))
         titles = [
             r'$q^2$',
             r'$\cos{\theta_k}$',
@@ -51,30 +50,37 @@ with bmf.Script() as script:
             ax.set(title=title)
         plt.show()
 
-        optimizer = bmf.Optimizer(fit_coeffs, signal_events)
+        attempt = 1
+        converged = False
+        while not converged:
+            fit_coeffs = bmf.coeffs.fit()
+            optimizer = bmf.Optimizer(fit_coeffs, signal_events)
 
-        def print_step():
-            bmf.stdout(
-                "Iteration:", "{}/{}". format(writer.current_id + 1, iterations),
-                "Step:", optimizer.step,
-                "Still training:", "{}/{}".format(optimizer.num_remaining(), len(optimizer.trainables)),
-                "normalized_nll:", optimizer.normalized_nll,
-            )
-            bmf.stdout("fit:   ", bmf.coeffs.to_str(fit_coeffs))
-            bmf.stdout("signal:", bmf.coeffs.to_str(signal_coeffs))
+            def print_step():
+                bmf.stdout(
+                    "Iteration:", "{}/{}". format(iteration, iterations),
+                    "Attempt", attempt,
+                    "Step:", optimizer.step,
+                    "Still training:", "{}/{}".format(optimizer.num_remaining(), len(optimizer.trainables)),
+                    "normalized_nll:", optimizer.normalized_nll,
+                )
+                bmf.stdout("fit:   ", bmf.coeffs.to_str(fit_coeffs))
+                bmf.stdout("signal:", bmf.coeffs.to_str(signal_coeffs))
 
-        print_step()
+            print_step()
 
-        while True:
-            optimizer.minimize()
-            if log:
-                log.coefficients('fit_{}'.format(writer.current_id + 1), optimizer, signal_coeffs)
-            if optimizer.converged():
-                print_step()
-                writer.write_coeffs(fit_coeffs)
-                break
-            if optimizer.step.numpy() % 100 == 0:
-                print_step()
-            if optimizer.step >= step_restart:
-                bmf.stderr('No convergence after {} steps. Restarting iteration'.format(step_restart))
-                break
+            while True:
+                optimizer.minimize()
+                if log:
+                    log.coefficients('fit_{}'.format(iteration), optimizer, signal_coeffs)
+                if optimizer.converged():
+                    converged = True
+                    print_step()
+                    writer.write_coeffs(fit_coeffs)
+                    break
+                if optimizer.step.numpy() % 100 == 0:
+                    print_step()
+                if optimizer.step >= step_restart:
+                    bmf.stderr('No convergence after {} steps. Restarting iteration'.format(step_restart))
+                    attempt = attempt + 1
+                    break

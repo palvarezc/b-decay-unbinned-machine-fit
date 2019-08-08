c56229166f81e856e1dd5057a8c4918bded76f56
Liam Shepherd <liam@dark.ms>
2019-08-08 16:02:30 +0100

    Added plotting of pulls

--- bin/plot_fits.py
+++ bin/plot_fits.py
@@ -5,6 +5,7 @@ Plot histograms of CSV result files.
 
 import argparse
 import csv
+import itertools
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy as sp
@@ -15,8 +16,6 @@ import tensorflow.compat.v2 as tf
 import b_meson_fit as bmf
 
 tf.enable_v2_behavior()
-
-sns.set_palette("Set2")
 
 
 def name_and_filename(arg):
@@ -78,35 +77,67 @@ with bmf.Script(device=None) as script:
 
     # For each amplitude
     for a_idx in range(0, bmf.coeffs.amplitude_count):
-        fig, axes = plt.subplots(bmf.coeffs.param_count)
+        fig, axes = plt.subplots(bmf.coeffs.param_count * 2)
         fig.suptitle(bmf.coeffs.amplitude_latex_names[a_idx])
 
         # For each param in this amplitude
         for p_idx in range(0, bmf.coeffs.param_count):
             c_idx = a_idx * bmf.coeffs.param_count + p_idx
+            colors = itertools.cycle(sns.color_palette("Set2"))
 
             drawn_something = False
             for name, points in data_points[bmf.coeffs.names[c_idx]].items():
                 if not all(elem == 0.0 for elem in points):
                     mean = np.mean(points)
                     std_err = sp.stats.sem(points, axis=None)
+                    pull = list(map(lambda p: (p - signal_coeffs[c_idx].numpy()) / std_err, points))
+                    pull_mean = np.mean(points)
+
                     bmf.stdout(
-                        '{}/{} signal: {} mean: {} std err: {}'.format(
+                        '{}/{} signal: {} mean: {} std err: {} pull mean: {}'.format(
                             bmf.coeffs.names[c_idx],
                             name,
                             signal_coeffs[c_idx].numpy(),
                             mean,
-                            std_err
+                            std_err,
+                            pull_mean,
                         )
                     )
-                    sns.distplot(points, ax=axes[p_idx], label=name, bins=args.bins, kde=False, norm_hist=True)
+                    color = next(colors)
+                    # Fit distribution
+                    sns.distplot(
+                        points,
+                        ax=axes[p_idx * 2],
+                        label=name,
+                        bins=args.bins,
+                        kde=False,
+                        norm_hist=True,
+                        color=color
+                    )
+                    # Fit pull
+                    sns.distplot(
+                        pull,
+                        ax=axes[p_idx * 2 + 1],
+                        label=name,
+                        bins=args.bins,
+                        kde=False,
+                        norm_hist=True,
+                        color=color
+                    )
+                    # Draw a darker line to represent the pull mean
+                    color_darker = tuple(map(lambda c: c * 0.5, color))
+                    axes[p_idx * 2 + 1].axvline(np.mean(pull), ymax=0.25, color=color_darker)
+
                     drawn_something = True
 
-            axes[p_idx].set_xlim(-args.plot_scale, args.plot_scale)
-            axes[p_idx].set_ylabel('density')
+            axes[p_idx * 2].set_xlim(-args.plot_scale, args.plot_scale)
+            axes[p_idx * 2].set_ylabel('density')
             if drawn_something:
-                axes[p_idx].legend()
+                axes[p_idx * 2].legend()
+                axes[p_idx * 2 + 1].legend()
                 # Draw a red line to represent the signal
-                axes[p_idx].axvline(signal_coeffs[c_idx].numpy(), ymax=0.25, color='r')
+                axes[p_idx * 2].axvline(signal_coeffs[c_idx].numpy(), ymax=0.25, color='r')
+                # Draw a black line to represent the mean
+                axes[p_idx * 2].axvline(mean, ymax=0.5, color='black')
 
         plt.show()

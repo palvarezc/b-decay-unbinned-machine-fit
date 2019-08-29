1fcb1df9c30d85489f49719234245b6e5df0e3d9
Liam Shepherd <liam@dark.ms>
2019-08-29 15:38:36 +0100

    Generate plots for all signal models

--- bin/run_plots.sh
+++ bin/run_plots.sh
@@ -2,13 +2,28 @@
 
 set -e
 
-./bin/plot_amplitudes.py --signal-model SM --write-svg 'results/amplitude-SM-%name%.svg'
-./bin/plot_angular_observables.py --signal-model SM --write-svg 'results/angular_observable-SM-%name%.svg'
 ./bin/plot_breit_wigner.py --write-svg results/breit_wigner.svg
-./bin/plot_frac_s.py --signal-model SM --write-svg results/frac_s-SM.svg
-./bin/plot_differential_decay_rate.py --signal-model SM --write-svg results/differential_decay_rate-SM.svg
-./bin/plot_signal.py --signal-model SM --write-svg 'results/signal-SM-%name%.svg'
-./bin/plot_confidence.py --write-svg 'results/confidence-SM-%name%.svg' results/SM_rate-0.05_b1-def_b2-def_eps-def_fi-def.csv
+for model in SM NP
+do
+    ./bin/plot_amplitudes.py \
+        --signal-model ${model} \
+        --write-svg "results/amplitude-${model}-%name%.svg"
+    ./bin/plot_angular_observables.py \
+        --signal-model ${model} \
+        --write-svg "results/angular_observable-${model}-%name%.svg"
+    ./bin/plot_frac_s.py \
+        --signal-model ${model} \
+        --write-svg results/frac_s-${model}.svg
+    ./bin/plot_differential_decay_rate.py \
+        --signal-model ${model} \
+        --write-svg results/differential_decay_rate-${model}.svg
+    ./bin/plot_signal.py \
+        --signal-model ${model} \
+        --write-svg "results/signal-${model}-%name%.svg"
+    ./bin/plot_confidence.py \
+        --write-svg "results/confidence-${model}-%name%.svg" \
+        results/${model}_rate-0.05_b1-def_b2-def_eps-def_fi-def.csv
+done
 for signal_count in 600 2400
 do
     ./bin/plot_q_test_statistic.py \

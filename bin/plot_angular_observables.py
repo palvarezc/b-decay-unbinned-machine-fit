7e5e844ae21e6fbf35a4747b796f6e997cf3a373
Liam Shepherd <liam@dark.ms>
2019-08-31 18:14:02 +0100

    Tweaked label in angular observable plots

--- bin/plot_angular_observables.py
+++ bin/plot_angular_observables.py
@@ -62,23 +62,23 @@ with bmf.Script(device=args.device) as script:
 
     # Observables
     observables = [
-        ('j1s', r'$j_{1s}$', bmf.signal.j1s(amplitudes, beta2, four_mass2_over_q2)),
-        ('j1c', r'$j_{1c}$', bmf.signal.j1c(amplitudes, four_mass2_over_q2)),
-        ('j2s', r'$j_{2s}$', bmf.signal.j2s(amplitudes, beta2)),
-        ('j2c', r'$j_{2c}$', bmf.signal.j2c(amplitudes, beta2)),
-        ('j3', r'$j_3$', bmf.signal.j3(amplitudes, beta2)),
-        ('j4', r'$j_4$', bmf.signal.j4(amplitudes, beta2)),
-        ('j5', r'$j_5$', bmf.signal.j5(amplitudes, beta)),
-        ('j6s', r'$j_{6s}$', bmf.signal.j6s(amplitudes, beta)),
-        ('j7', r'$j_7$', bmf.signal.j7(amplitudes, beta)),
-        ('j8', r'$j_8$', bmf.signal.j8(amplitudes, beta2)),
-        ('j9', r'$j_9$', bmf.signal.j9(amplitudes, beta2)),
-        ('jp1c', r'$j_{1c}^{\prime}$', bmf.signal.j1c_prime(amplitudes)),
-        ('jpp1c', r'$j_{1c}^{\prime\prime}$', bmf.signal.j1c_dblprime(amplitudes)),
-        ('jp4', r'$j_4^{\prime}$', bmf.signal.j4_prime(amplitudes)),
-        ('jp5', r'$j_5^{\prime}$', bmf.signal.j5_prime(amplitudes)),
-        ('jp7', r'$j_7^{\prime}$', bmf.signal.j7_prime(amplitudes)),
-        ('jp8', r'$j_8^{\prime}$', bmf.signal.j8_prime(amplitudes)),
+        ('j1s', r'$J_{1s}$', bmf.signal.j1s(amplitudes, beta2, four_mass2_over_q2)),
+        ('j1c', r'$J_{1c}$', bmf.signal.j1c(amplitudes, four_mass2_over_q2)),
+        ('j2s', r'$J_{2s}$', bmf.signal.j2s(amplitudes, beta2)),
+        ('j2c', r'$J_{2c}$', bmf.signal.j2c(amplitudes, beta2)),
+        ('j3', r'$J_3$', bmf.signal.j3(amplitudes, beta2)),
+        ('j4', r'$J_4$', bmf.signal.j4(amplitudes, beta2)),
+        ('j5', r'$J_5$', bmf.signal.j5(amplitudes, beta)),
+        ('j6s', r'$J_{6s}$', bmf.signal.j6s(amplitudes, beta)),
+        ('j7', r'$J_7$', bmf.signal.j7(amplitudes, beta)),
+        ('j8', r'$J_8$', bmf.signal.j8(amplitudes, beta2)),
+        ('j9', r'$J_9$', bmf.signal.j9(amplitudes, beta2)),
+        ('jp1c', r'$J_{1c}^{\prime}$', bmf.signal.j1c_prime(amplitudes)),
+        ('jpp1c', r'$J_{1c}^{\prime\prime}$', bmf.signal.j1c_dblprime(amplitudes)),
+        ('jp4', r'$J_4^{\prime}$', bmf.signal.j4_prime(amplitudes)),
+        ('jp5', r'$J_5^{\prime}$', bmf.signal.j5_prime(amplitudes)),
+        ('jp7', r'$J_7^{\prime}$', bmf.signal.j7_prime(amplitudes)),
+        ('jp8', r'$J_8^{\prime}$', bmf.signal.j8_prime(amplitudes)),
     ]
 
     for observable in observables:
@@ -88,8 +88,8 @@ with bmf.Script(device=args.device) as script:
 
         plt.plot(q2.numpy(), values.numpy())
         plt.margins(x=0)
-        plt.title(latex_name)
         plt.xlabel(r'$q^2 / (GeV^2 / c^4)$')
+        plt.ylabel(latex_name)
         if args.write_svg is not None:
             filepath = args.write_svg.replace('%name%', name)
             bmf.stdout('Writing {}'.format(filepath))
